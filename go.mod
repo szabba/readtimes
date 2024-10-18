32800216291a70301b828945de01ef24c5e102c3
Karol Marcjan <karol.marcjan@prelift.dev>
2024-10-19 00:12:38 +0200

    main.go: Print out time following a configurable schedule (in a loop)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/szabba/readtime
 
 go 1.23.2
+
+require github.com/robfig/cron/v3 v3.0.1
--- main.go
+++ main.go
@@ -2,17 +2,65 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
-var format string = "15:04"
+var (
+	format string
+	sched  Schedule
+)
 
 func main() {
 
 	flag.StringVar(&format, "fmt", "15 04", "Time format to use. Specified as a Go time package layout.")
 
+	flag.Var(&sched, "sched", "A cron-like schedule. Minutes and hours are mandatory. Optionally, seconds can be specified.")
+	sched.MustSet("0 *")
+
 	flag.Parse()
 
-	fmt.Printf("It is now %s.", time.Now().Format(format))
+	for {
+		log.Printf("It is now %s.", time.Now().Format(format))
+
+		next := sched.Next()
+		left := time.Until(next)
+
+		<-time.After(left)
+	}
 }
+
+type Schedule struct {
+	raw    string
+	parsed cron.Schedule
+}
+
+func (s *Schedule) Next() time.Time {
+	now := time.Now()
+	return s.parsed.Next(now)
+}
+
+func (s *Schedule) MustSet(raw string) {
+	err := s.Set(raw)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var _ flag.Value = new(Schedule)
+
+func (s *Schedule) Set(raw string) error {
+	p := cron.NewParser(cron.SecondOptional | cron.Minute | cron.Hour)
+	parsed, err := p.Parse(raw)
+
+	if err != nil {
+		return err
+	}
+
+	s.raw, s.parsed = raw, parsed
+	return nil
+}
+
+func (s *Schedule) String() string { return s.raw }
